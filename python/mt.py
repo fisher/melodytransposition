c4e00b7089d8c7ad1d19ce89744a1c64e4a10f72
Serge A. Ribalchenko <fisher@heim.in.ua>
2014-03-10 18:52:30 +0200

    just a skeleton using getopt

--- python/mt.py
+++ python/mt.py
@@ -1,1 +1,33 @@
-#!/bin/env python
+#!/usr/bin/env python
+#
+
+
+import getopt, sys
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    output = None
+    verbose = False
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-o", "--output"):
+            output = a
+        else:
+            assert False, "unhandled option"
+    # ...
+
+def usage():
+    print "usage: mt.py <melody spec>"
+    
+if __name__ == "__main__":
+    main()
