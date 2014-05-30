504707448ceb1fba494d7788bf4c8e12a00e53b6
Serge Ribalchenko <fisher@tpaba.org.ua>
2014-05-30 15:33:07 +0300

    added check for no args case

--- python/mt.py
+++ python/mt.py
@@ -20,6 +20,9 @@ def qert():
     print "melody : ", args
 
 def main():
+    if len(sys.argv) <2:
+        usage()
+        sys.exit(2)
     try:
         opts, args = getopt.getopt(sys.argv[1:], "ho:vVb",
                                    ["help", "output=", "verbose", "vertical", "bias=", "version"])
@@ -41,14 +44,14 @@ def main():
         elif o in ("-V", "--vertical"):
             vertical = True
         elif o in ("--version"):
-            print sys.argv[0], ", python2/getopt version (python)"
+            print sys.argv[0], "x, python2/getopt version (python)"
             sys.exit()
         else:
             assert False, "FIXME: unhandled option"
     qert()
 
 def usage():
-    print "usage: ", sys.argv[0], " <melody spec>"
+    print "usage: ", sys.argv[0], " [options] <melody spec>"
 
 if __name__ == "__main__":
     main()
