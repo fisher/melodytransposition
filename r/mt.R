5bed999c2acd2ede91e9de2b0663d7e888f5cda3
Serge A. Ribalchenko <fisher@heim.in.ua>
2015-03-12 00:23:44 +0200

    getopt cannot be used

--- r/mt.R
+++ r/mt.R
@@ -13,17 +13,26 @@
 # $./example.R --array_id 42
 # $./example.R --array_id $SLURM_ARRAYID
 
+# can't use it:
+#6. No support for mixing in positional arguments or extra arguments
+# that don’t match any options.  For example, you can’t do
+# "my.R –arg1 1 foo bar baz" and recover "foo", "bar", "baz" as a list.
+# Likewise for "my.R foo –arg1 1 bar baz".
 library(getopt);
 
-opt = getopt(matrix(c(
-	'help', 'h', 0, "logical", "Usage information",
-	'array_id', 'a', 1, "integer", "Value of the var"
-	), ncol=5, byrow=TRUE));
+spec = matrix(
+    c(
+      'help', 'h', 0, "logical", "Usage information",
+	  'array_id', 'a', 1, "integer", "Value of the var"
+	), ncol=5, byrow=TRUE);
+
+opt = getopt(spec);
 
 if (!is.null(opt$help)) {
-	writeLines("Usage: mt.R [options]");
-	writeLines("\t--help,h\tPrint usage options");
-	writeLines("\t--array_id,a\tValue of var");
+    cat(getopt(spec, usage=TRUE));
+	#writeLines("Usage: mt.R [options]");
+	#writeLines("\t--help,h\tPrint usage options");
+	#writeLines("\t--array_id,a\tValue of var");
 
 	q(status=1);
 }
