aa09a72319a67931c93214558e77dd464d9eee04
Serge A. Ribalchenko <fisher@heim.in.ua>
2015-03-30 20:50:42 +0300

    using argparser

--- r/mt.R
+++ r/mt.R
@@ -3,29 +3,31 @@
 
 ## doc for this module:
 ## http://cran.r-project.org/web/packages/argparser/argparser.pdf
-if(!require("argparser")) {
+if(!require("argparser", quietly=TRUE)) {
     install.packages("argparser")
     require("argparser")
 }
 
-spec = matrix(
-    c(
-      'help', 'h', 0, "logical", "Usage information",
-	  'array_id', 'a', 1, "integer", "Value of the var"
-	), ncol=5, byrow=TRUE);
+args <- commandArgs(trailingOnly = TRUE)
 
-opt = getopt(spec);
+parser <- arg.parser("Melody transposition programm in R language")
+parser <- add.argument(parser, "--verbose", help="show details", flag=TRUE)
+parser <- add.argument(parser, "--bias", help="bias in semitones", default=0)
+parser <- add.argument(parser, "--vertical", help="vertical alignment", flag=TRUE)
+parser <- add.argument(parser, "--version", help="show the version", flag=TRUE)
+parser <- add.argument(parser, "--output", help="output format", default="letters")
 
-if (!is.null(opt$help)) {
-    cat(getopt(spec, usage=TRUE));
-	#writeLines("Usage: mt.R [options]");
-	#writeLines("\t--help,h\tPrint usage options");
-	#writeLines("\t--array_id,a\tValue of var");
 
-	q(status=1);
+options = parse.args(parser, argv= args);
+
+if ( options$version ) {
+    print ("r script version")
+    q(status=0)
 }
 
-print (opt$array_id);
+if ( length(args) ==0 ) { ## no melody specified
+    print(parser)
+    q(status=1)
+}
 
-n<-opt$array_id;
-
+print (args)
